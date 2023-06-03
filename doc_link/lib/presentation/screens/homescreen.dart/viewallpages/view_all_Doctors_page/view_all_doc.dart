f63c26ca6d842e52fb4935a1bfc0f83d982993bb
sufad M <sufadm77@gmail.com>
2023-06-03 18:09:24 +0530

    completed ui of doctors list

--- doc_link/lib/presentation/screens/homescreen.dart/viewallpages/view_all_Doctors_page/view_all_doc.dart
+++ doc_link/lib/presentation/screens/homescreen.dart/viewallpages/view_all_Doctors_page/view_all_doc.dart
@@ -1,3 +1,5 @@
+import 'package:doc_link/const/const.dart';
+import 'package:doc_link/presentation/screens/homescreen.dart/viewallpages/view_all_Doctors_page/widgets/doctors_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class ViewAllDoctorsPage extends StatelessWidget {
@@ -6,22 +8,55 @@ class ViewAllDoctorsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      elevation: 0,
-      iconTheme: const IconThemeData(
-        color: Colors.black, // Change your color here
-      ),
-      backgroundColor: Colors.white,
-      title: const Text(
-        'All Doctors',
-        style: TextStyle(
-          color: Colors.black, // Change the text color here
-          fontSize: 20, // Adjust the font size if needed
-          fontWeight: FontWeight.bold, // Adjust the font weight if needed
+      appBar: AppBar(
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black, // Change your color here
+        ),
+        backgroundColor: Colors.white,
+        title: const Text(
+          'All Doctors',
+          style: TextStyle(
+            color: Colors.black, // Change the text color here
+            fontSize: 20, // Adjust the font size if needed
+            fontWeight: FontWeight.bold, // Adjust the font weight if needed
+          ),
         ),
       ),
-    )
-        //body: ,
-        );
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            kHeight15,
+            SearchBar(
+              hintText: 'Search doctor',
+              leading: Container(
+                  margin: const EdgeInsets.only(left: 10),
+                  child: const Icon(Icons.search)),
+            ),
+            kHeight15,
+            const DoctorsListWidget(
+              doctorname: 'Doctor name 1',
+            ),
+            kHeight10,
+            const DoctorsListWidget(
+              doctorname: 'Doctor name 2',
+            ),
+            kHeight10,
+            const DoctorsListWidget(
+              doctorname: 'Doctor name 3',
+            ),
+            kHeight10,
+            const DoctorsListWidget(
+              doctorname: 'Doctor name 4',
+            ),
+            kHeight10,
+            const DoctorsListWidget(
+              doctorname: 'Doctor name 5',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ doc_link/lib/presentation/screens/homescreen.dart/viewallpages/view_all_Doctors_page/widgets/doctors_list_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../const/const.dart';
+
+class DoctorsListWidget extends StatelessWidget {
+  final String doctorname;
+  const DoctorsListWidget({
+    super.key,
+    required this.doctorname,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(255, 184, 184, 184),
+        ),
+        height: 70,
+        width: double.infinity,
+        child: Container(
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const CircleAvatar(
+                maxRadius: 30,
+                minRadius: 10,
+              ),
+              Text(
+                doctorname,
+                style: kTextStyleMediumBlack,
+              ),
+              const Icon(Icons.east)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
