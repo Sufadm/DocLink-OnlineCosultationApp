c6adfd75a60422f93af70cfc86c8c530a9b514b7
sufad M <sufadm77@gmail.com>
2023-06-05 12:03:04 +0530

    setup firebise to my app

--- doc_link/lib/main.dart
+++ doc_link/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:doc_link/firebase_options.dart';
 import 'package:doc_link/presentation/screens/spashscreen/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
